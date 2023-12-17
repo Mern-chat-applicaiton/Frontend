6eb41fcbbae0105e35e86cef035ff0b8bb7263c0
Manikanta chanda <108612806+manikantachanda@users.noreply.github.com>
2023-12-17 22:58:07 +0530

    Update SideDrawer.js

--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -149,28 +149,8 @@ function SideDrawer() {
         <div>
           <Menu>
             <MenuButton p={1}>
-              // <NotificationBadge
-              //   count={notification.length}
-              //   effect={Effect.SCALE}
-              // />
               <BellIcon fontSize="2xl" m={1} />
             </MenuButton>
-            // <MenuList pl={2}>
-            //   {!notification.length && "No New Messages"}
-            //   {notification.map((notif) => (
-            //     <MenuItem
-            //       key={notif._id}
-            //       onClick={() => {
-            //         setSelectedChat(notif.chat);
-            //         setNotification(notification.filter((n) => n !== notif));
-            //       }}
-            //     >
-            //       {notif.chat.isGroupChat
-            //         ? `New Message in ${notif.chat.chatName}`
-            //         : `New Message from ${getSender(user, notif.chat.users)}`}
-            //     </MenuItem>
-            //   ))}
-            // </MenuList>
           </Menu>
           <Menu>
             <MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
